77312f5fcab1786d9122e180b186d20810474290
Erica Kastner <dkastner@gmail.com>
2024-11-23 00:31:06 -0600

    fix title or something

--- lib/beats/destination_track_file.rb
+++ lib/beats/destination_track_file.rb
@@ -7,6 +7,13 @@ module Beats
       super path: nil, album: album, track: track, ext: ext
     end
 
+    def filename
+      return @filename unless @filename.nil?
+
+      track_title = Sanitize.filename(track.title.to_s)
+      @filename = [track.number.to_s.rjust(2,'0'), track_title].join(' - ')
+    end
+
     def path
       @path ||= File.join(TRACKS_PATH, Sanitize.filename(album.artist_title), filename) + ext
     end
